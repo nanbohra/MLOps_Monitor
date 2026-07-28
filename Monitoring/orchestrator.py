cfb8892d4cc5338754cc60329a08d168f900fd2e
nanbohra <nbohra@ucsd.edu>
2026-07-28 11:59:44 +0800

    Planned methods for orchestrator, trying to figure out flow

--- Monitoring/orchestrator.py
+++ Monitoring/orchestrator.py
@@ -20,7 +20,7 @@ class Orchestrator:
         self.scaler = joblib.load(config['scaler_path'])
 
         baseline_df = pd.read_csv(config['baseline_data_path']) # all the data accumulated so far
-        self.seen_data = baseline_df
+        self.seen_data = [baseline_df]
 
         # init monitors 
         self.drift_detector = DriftDetector(baseline_df, self.features)
@@ -44,3 +44,68 @@ class Orchestrator:
 
         # history
         self.batch_log = []
+
+    def process_batch():
+        # TODO
+        # records batch data into seen_data unconditionally for eventual retraining
+        # triggers monitors based on state (monitoring vs. shadow)
+        # sends batch records from ^ to log
+
+    # support functions
+    def _run_monitoring():
+        # TODO
+        # feed data features into drift detector
+        # feed predictions vs ground truths into performance eval
+        # feed detector data into alert manager
+
+        # collate specific information into a record for logging
+        # trigger retraining if required
+
+
+    def _predict():
+        # TODO
+        # useful for shadow testing with parameterized model pieces
+        # run inference on both models concurrently
+
+    
+    def _start_retrain():
+        # TODO
+        # collates training data to send to retrainer
+        # trigger retraining with imported function
+        # handles state change
+    
+    def _all_seen_data():
+        # TODO
+        # concat all batch data in seen_data list for eventual retraining
+    
+    def _run_shadow_test():
+        # TODO
+        # collect feature data + ground truths 
+        # run drift detection on data, but not passed to alert manager, since alerts are paused during shadow testing
+        # run both models through prediction / inference
+        # record performance of production model (performanceEva)
+        # get all metrics on challenger model also
+        # create logs for all sensors
+        # if we've run teh challenger for enough test batches, send to promotion evaluation
+
+    def _evaluate_promotion():
+        # TODO
+        # evaluate metrics across shadow testing to see which model prevails
+        # CURRENT CRITERION FOR PROMOTION: improved avg f1
+        # check against config threshold if avg f1 has improved 
+        # replace production model with retrained model
+        # update reference data, notify retrain on performanceEval
+        # reset alert manager
+        # reset initializations for shadow challenger
+        # reset state
+        # ELSE discard challenger if it didn't outperform
+            # retraining will be re-attempted after cooldown
+
+    def save_log():
+        # TODO
+        # save logs to CSV
+    
+    
+
+
+    
